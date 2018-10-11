8decdf934134e84868413c7e23fef819a7a9f80b
Luka Klacar <lukaklacar@gmail.com>
2018-10-11 15:10:49 +0200

    Use global package

--- src/new/NewApplicationGenerator.ts
+++ src/new/NewApplicationGenerator.ts
@@ -1,8 +1,7 @@
 import chalk from 'chalk'
 // @ts-ignore
 import chalkAnimation from 'chalk-animation'
-import * as path from 'path'
-import { BaseGenerator, getNodeModulesDirectory, execute } from '@hyke/sdk'
+import { BaseGenerator, execute } from '@hyke/sdk'
 
 interface IArgs {
   appName: string
@@ -12,13 +11,7 @@ interface IArgs {
 export default class NewApplicationGenerator extends BaseGenerator<IArgs> {
   public generate(args: IArgs): void {
     chalkAnimation.rainbow('Generating React Native application. Please wait...')
-    const reactNativeCliPath = path.join(getNodeModulesDirectory(), 'react-native-cli', 'index.js')
-    execute(
-      'node',
-      [reactNativeCliPath, 'init', args.appName, '--template', 'hike'],
-      {},
-      args.verbose
-    )
+    execute('node', ['react-native', 'init', args.appName, '--template', 'hike'], {}, args.verbose)
       .then(() => execute('node', [`${args.appName}/setup.js`], {}, args.verbose))
       .then(() => console.log(chalk.green('Application successfully generated.')))
       .catch(() => {
